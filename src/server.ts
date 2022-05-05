15e0b78fdfca951732532e13ed68d4d6628830bc
Bruna Flôr <brn.flor@gmail.com>
2022-05-05 18:39:27 +0100

    server and mail complete

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,18 @@
+import { prisma } from './prisma';
 import express from 'express'
+import { routes } from './routes';
 
 const app=express();
 
+app.use(express.json())
+app.use(routes);
 
-app.get('/users', (req, res) => {
-return res.send('Hello world')})
+
+
+
+
 
 
 app.listen(3333, ()=> {
-    console.log('HTTP server running!')
+    console.log('HTTP server running!');
 });
---
+++ src/use-cases/submit-feedback-use-case-spec.ts
@@ -0,0 +1,5 @@
+// import { hasUncaughtExceptionCaptureCallback } from "process"
+
+// test('sum 2+2', ()=> {
+//     hasUncaughtExceptionCaptureCallback(2+2).toBe(4)
+// });
